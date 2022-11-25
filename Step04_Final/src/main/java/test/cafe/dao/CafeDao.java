090a2cd8027bf8fb7441872820a66a8ff36f6a17
CheonOkHui <cjsdhrgml000@gmail.com>
2022-11-25 15:55:54 +0900

    Step04에 삭제 수정 페이징처리 구현함

--- Step04_Final/src/main/java/test/cafe/dao/CafeDao.java
+++ Step04_Final/src/main/java/test/cafe/dao/CafeDao.java
@@ -34,6 +34,81 @@ public class CafeDao {
 		return dao;
 	}
 	
+	// 글의 조회수를 올리는 메소드
+	public boolean addViewCount(int num) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+
+		int rowCount = 0;
+
+		try {
+			conn = new DbcpBean().getConn();
+			String sql = "UPDATE board_cafe"
+					+ " SET viewCount = viewCount+1"
+					+ " WHERE num = ?";
+
+			pstmt = conn.prepareStatement(sql);
+			// ?에 바인딩할게 있으면 해주고
+			pstmt.setInt(1, num);
+			
+			// INSERT OR UPDATE OR DELETE 문을 수행하고 수정되거나, 삭제되거나, 추가된 ROW의 갯수 리턴 받기
+			rowCount = pstmt.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		if (rowCount > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	// 글하나의 정보를 삭제하는 메소드
+	public boolean delete(int num) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+
+		int rowCount = 0;
+
+		try {
+			conn = new DbcpBean().getConn();
+			String sql = "DELETE FROM board_cafe"
+					+ " WHERE num = ?";
+
+			pstmt = conn.prepareStatement(sql);
+			// ?에 바인딩할게 있으면 해주고
+			pstmt.setInt(1, num);
+			
+			// INSERT OR UPDATE OR DELETE 문을 수행하고 수정되거나, 삭제되거나, 추가된 ROW의 갯수 리턴 받기
+			rowCount = pstmt.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		if (rowCount > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
 	// 글 하나의 정보를 수정하는 메소드
 	public boolean update(CafeDto dto) {
 		Connection conn = null;
@@ -156,7 +231,7 @@ public class CafeDao {
 		}
 	}
 	
-	public List<CafeDto> getList(){
+	public List<CafeDto> getList(CafeDto dto){
 		List<CafeDto> list = new ArrayList<>();
 		// 필요한 객체를 담을 지역변수를 미리 만들어 둔다.
 		Connection conn = null;
@@ -166,26 +241,34 @@ public class CafeDao {
 			// Connection Pool에서 Connection 객체를 하나 얻어온다.
 			conn = new DbcpBean().getConn();
 			// 실행할 sql 문의 뼈대 구성하기
-			String sql = "SELECT num, writer, title, content, viewCount, regdate"
-					+ " FROM board_cafe"
-					+ " ORDER BY num DESC";
+			String sql = "SELECT *"
+					+ " FROM"
+					+ "	(SELECT result1.*, ROWNUM AS RNUM"
+					+ "	FROM"
+					+ "		(SELECT num, writer, title, content, viewCount, TO_CHAR(regdate, 'YYYY.MM.DD HH24:MI') AS regdate"
+					+ "		FROM board_cafe"
+					+ "		ORDER BY num DESC) result1)"
+					+ " WHERE rnum BETWEEN ? AND ?";
+			
 			// sql 문의 ?에 바인딩 할 게 있으면 한다.
-
 			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, dto.getStartRowNum());
+			pstmt.setInt(2, dto.getEndRowNum());
+			
 			// SELECT문을 수행하고 결과값을 받아온다.
 			rs = pstmt.executeQuery();
 			// 반복문 돌면서 ResultSet에서 필요한 값을 얻어낸다.
 			while (rs.next()) {
 				//select 된 row 하나의 정보를 CafeDto 객체를 생성해서 담고 
-				CafeDto dto = new CafeDto();
-				dto.setNum(rs.getInt("num"));
-				dto.setWriter(rs.getString("writer"));
-				dto.setTitle(rs.getString("title"));
-				dto.setContent(rs.getString("content"));
-				dto.setViewCount(rs.getInt("viewCount"));
-				dto.setRegdate(rs.getString("regdate"));
+				CafeDto tmp = new CafeDto();
+				tmp.setNum(rs.getInt("num"));
+				tmp.setWriter(rs.getString("writer"));
+				tmp.setTitle(rs.getString("title"));
+				tmp.setContent(rs.getString("content"));
+				tmp.setViewCount(rs.getInt("viewCount"));
+				tmp.setRegdate(rs.getString("regdate"));
 				//CafeDto 객체의 참조값을 List 에 누적 시키기
-				list.add(dto);
+				list.add(tmp);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -201,6 +284,44 @@ public class CafeDao {
 			}
 		}
 		return list;
+	}
+	
+	// 전체 글의 갯수를 리턴하는 메소드
+	public int getCount() {
+		int count = 0;
+		// 필요한 객체를 담을 지역변수를 미리 만들어 둔다.
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			// Connection Pool에서 Connection 객체를 하나 얻어온다.
+			conn = new DbcpBean().getConn();
+			// 실행할 sql 문의 뼈대 구성하기
+			String sql = "SELECT MAX(ROWNUM) AS num"
+					+ " FROM board_cafe";
+			// sql 문의 ?에 바인딩 할 게 있으면 한다.
+
+			pstmt = conn.prepareStatement(sql);
+			// SELECT문을 수행하고 결과값을 받아온다.
+			rs = pstmt.executeQuery();
+			// 반복문 돌면서 ResultSet에서 필요한 값을 얻어낸다.
+			if (rs.next()) {
+				count = rs.getInt("num");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+			}
+		}
+		return count;
 	}
 
 }
--- Step04_Final/src/main/webapp/cafe/list.jsp
+++ Step04_Final/src/main/webapp/cafe/list.jsp
@@ -4,9 +4,50 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%
-	// 글 목록 얻어오기
-	List<CafeDto> list = CafeDao.getInstance().getList();
+	
 
+	// 한 페이지에 몇 개씩 표시할 것인지
+	final int PAGE_ROW_COUNT = 5;
+	// 하단 페이지를 몇개씩 표시할 것인지
+	final int PAGE_DISPLAY_COUNT = 5;
+	
+	// 보여줄 페이지의 번호를 일단 1이라고 초기값 지정
+	int pageNum = 1;
+	
+	// 페이지 번호가 파라미터로 전달되는지 읽어와 본다
+	String strPageNum = request.getParameter("pageNum");
+	// 만일 페이지 번호가 파라미터로 넘어온다면
+	if(strPageNum != null){
+		// 숫자로 바꿔서 보여줄 페이지 번호로 지정한다.
+		pageNum = Integer.parseInt(strPageNum);
+	}
+	
+	// 보여줄 페이지의 시작 ROWNUM
+	int startRowNum = 1+(pageNum - 1)*PAGE_ROW_COUNT;
+	// 보여줄 페이지의 끝 ROWNUM	
+	int endRowNum = pageNum * PAGE_ROW_COUNT;
+	
+	// 하단 시작 페이지 번호
+	int startPageNum = 1 + ((pageNum-1)/PAGE_DISPLAY_COUNT)*PAGE_DISPLAY_COUNT;
+	// 하단 끝 페이지 번호
+	int endPageNum = startPageNum + PAGE_DISPLAY_COUNT-1;
+	// 전체 글의 갯수
+	int totalRow = CafeDao.getInstance().getCount();
+	// 전체 페이지의 갯수 구하기
+	int totalPageCount = (int)Math.ceil(totalRow/(double)PAGE_ROW_COUNT);
+	// 끝 페이지 번호가 이미 전체 페이지 갯수보다 크게 계산되어있다면 잘못된 값이다.
+	if(endPageNum > totalPageCount){
+		endPageNum = totalPageCount; // 보장해준다.
+	}
+	
+	// CafeDto 객체를 생성해서
+	CafeDto dto = new CafeDto();
+	// 위에서 계산된 startRowNum, endRowNum을 담아서
+	dto.setStartRowNum(startRowNum);
+	dto.setEndRowNum(endRowNum);
+	
+	// CafeDto를 인자로 전달해서 글 목록 얻어오기
+	List<CafeDto> list = CafeDao.getInstance().getList(dto);
 %>
 <!DOCTYPE html>
 <html>
@@ -44,6 +85,31 @@
 				<%} %>
 			</tbody>
 		</table>
+		<nav>
+			  <ul class="pagination justify-content-center mt-5">
+			  <%--
+					startPageNum이 1이 아닌 경우에만 Prev 링크를 제공한다.
+				 --%>
+				 <%if(startPageNum != 1){ %>
+				 	<li class="page-item">
+				 		<a class="page-link" href="list.jsp?pageNum=<%=startPageNum-1 %>"> Prev </a>
+				 	</li>
+				 <%} %>
+			    <%for(int i=startPageNum; i<=endPageNum; i++){ %>
+					<li class="page-item <%=pageNum == i ? "active" : ""%>">
+						<a class="page-link" href="list.jsp?pageNum=<%=i %>"><%=i %> </a>
+					</li>				
+				<%} %>
+				<%--
+					마지막 페이지 번호가 전체 페이지의 갯수보다 작으면 Next 링크를 제공한다.
+				 --%>
+				 <% if(endPageNum < totalPageCount){ %>
+					<li class="page-item">
+						<a class="page-link" href="list.jsp?pageNum=<%=endPageNum+1 %>">Next</a>
+					</li>
+				<%} %>
+			  </ul>
+		</nav>
 	</div>
 </body>
 </html>
